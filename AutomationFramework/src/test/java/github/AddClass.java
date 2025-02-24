6df49b5a9e1ff2918236ac2ce7225098b065e42e
aditya8720 <aditya.shri.872@gmail.com>
2025-02-24 19:39:46 +0530

    add one class

---
+++ AutomationFramework/src/test/java/github/AddClass.java
@@ -0,0 +1,11 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AddClass {
+	@Test
+	private void demo() {
+		Reporter.log("add class added", true);
+	}
+}
